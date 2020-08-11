865eb0221214e70a2cae5daffe9907c492787699
Marco Crespi <marco.crespi@modum.io>
2020-08-11 13:14:52 +0200

    fix: Allow running in main process

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
 var Noble = require('./lib/noble');
-
-if(process.env.debug) {
-  console.lag = console.log;
-} else {
-  console.lag = () => {};
-}
-
-function isSlaveProcess() {
-
-  return (!!process.argv.find((arg) => {
-    return arg === "out-of-proc";
-  }));
-}
-
-var bindings;
-if(isSlaveProcess() || process.argv[1].includes("ws-slave")) {
-    bindings = require('./lib/resolve-bindings')();
-    module.exports = new Noble(bindings);
-} else {
-    module.exports = null;
-}
+var bindings = require('./lib/resolve-bindings')();
+module.exports = new Noble(bindings);
